444fa76262f94a2038f935730664203b85cc7769
Nicolas "Norswap" Laurent <norswap@gmail.com>
2015-08-22 16:42:54 +0200

    update java_template & remove results

--- java_template/update_gitignore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-cat $DIR/template.gitignore own.gitignore > .gitignore 2>/dev/null
--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 t?=com.norswap.autumn.test.Main
 LIBS=libs/java_utils/src
-include java_template/template.mk
+include library.mk
